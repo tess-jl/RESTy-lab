9f305c91b77f598a993c215952152f7784df88dd
Tess Lameyer <tjlameyer@gmail.com>
2020-01-31 15:44:32 -0800

    refactored fetch so that it is modular

--- src/containers/RESTy.js
+++ src/containers/RESTy.js
@@ -5,6 +5,8 @@ import Header from '../components/Header/Header.js';
 import Footer from '../components/Footer/Footer.js';
 import Result from '../components/Result/Result.js';
 
+const methodsWithBody = ['POST', 'PUT', 'PATCH'];
+
 export default class RESTy extends Component {
   state = {
     url: '', 
@@ -17,38 +19,32 @@ export default class RESTy extends Component {
     responseBody: ''
   }
 
+  fetchWithError = () => {
+    return fetch(this.state.url, { 
+      method: this.state.method,
+      headers: methodsWithBody.includes(this.state.method) ? { 'Content-Type': 'application/json', 'Authorization': `Bearer ${this.state.bearerToken}` } : {},
+      body: methodsWithBody.includes(this.state.method) ? this.state.rawJSONBody : null
+    })
+      .then(res => res.json())
+      .catch();
+  };
+
 
   handleSubmit = event => {
     event.preventDefault(); 
 
-    this.setState(state => ({
-      list: [...state.list, {
-        url: state.url, 
-        rawJSONBody: state.rawJSONBody, 
-        username: state.username, 
-        password: state.password, 
-        bearerToken: state.bearerToken, 
-        method: state.method
-      }]
-    })); 
-
-    let headers; 
-    let body; 
-    if(this.state.method !== 'GET') {
-      headers = {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${this.state.bearerToken}`
-      };
-      body = this.state.rawJSONBody;
-    } 
-
-    fetch(this.state.url, {
-      method: this.state.method, 
-      headers, 
-      body
-    })
-      .then(res => res.json())
-      .then(res => this.setState({ responseBody: JSON.stringify(res, null, 2) }));
+    this.fetchWithError()
+      .then(res => this.setState(state => ({
+        responseBody: JSON.stringify(res, null, 2),
+        list: [...state.list, {
+          url: state.url, 
+          rawJSONBody: state.rawJSONBody, 
+          username: state.username, 
+          password: state.password, 
+          bearerToken: state.bearerToken, 
+          method: state.method
+        }]
+      })));
   };
 
   handleChange = ({ target }) => {
@@ -62,7 +58,6 @@ export default class RESTy extends Component {
 
   handleClick = (listItem) => {
     this.setState(() => ({ url: listItem.url, method: listItem.method, rawJSONBody: listItem.rawJSONBody }));
-
   }; 
 
   render() {
